6060055efbb28e54e7cf302c155eb1a64e80b8da
Boris Raicheff <boris@MacBook-Pro.local>
2017-05-27 14:46:20 +0100

    Initial commit

---
+++ flask_bdea.py
@@ -0,0 +1,43 @@
+#
+# Flask-Keen
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+import warnings
+
+
+class BDEA(object):
+    """
+    Flask-BDEA
+
+    Documentation:
+    https://flask-bdea.readthedocs.io
+
+    :param app: Flask app to initialize with. Defaults to `None`
+    """
+
+    client = None
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        project_id = app.config.get('KEEN_PROJECT_ID')
+        if project_id is None:
+            warnings.warn('KEEN_PROJECT_ID not set', RuntimeWarning, stacklevel=2)
+            return
+        self.client = KeenClient(
+            project_id=project_id,
+            write_key=app.config.get('KEEN_WRITE_KEY'),
+            read_key=app.config.get('KEEN_READ_KEY'),
+        )
+
+    def __getattr__(self, name):
+        return getattr(self.client, name)
+
+
+# EOF
---
+++ setup.py
@@ -0,0 +1,24 @@
+#
+# Flask-Keen
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-BDEA',
+    version='0.1.0',
+    description='Flask-BDEA',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/flask-bdea',
+    install_requires=('flask', 'block-disposable-email'),
+    py_modules=('flask_keen',),
+)
+
+
+# EOF
