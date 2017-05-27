c00fb19daac9594eb0d46baf1348a873f67cddbc
Boris Raicheff <boris@MacBook-Pro.local>
2017-05-27 14:51:44 +0100

    Release 0.1.0

--- flask_bdea.py
+++ flask_bdea.py
@@ -8,6 +8,14 @@
 
 import warnings
 
+from bdea.client import BDEAClient
+from flask import current_app
+
+try:
+    from wtforms.validators import ValidationError
+except ImportError:
+    pass
+
 
 class BDEA(object):
     """
@@ -26,18 +34,36 @@ class BDEA(object):
             self.init_app(app)
 
     def init_app(self, app):
-        project_id = app.config.get('KEEN_PROJECT_ID')
-        if project_id is None:
-            warnings.warn('KEEN_PROJECT_ID not set', RuntimeWarning, stacklevel=2)
+        api_key = app.config.get('BDEA_API_KEY')
+        if api_key is None:
+            warnings.warn('BDEA_API_KEY not set', RuntimeWarning, stacklevel=2)
             return
-        self.client = KeenClient(
-            project_id=project_id,
-            write_key=app.config.get('KEEN_WRITE_KEY'),
-            read_key=app.config.get('KEEN_READ_KEY'),
-        )
+        self.client = BDEAClient(api_key)
+        app.extensions['bdea'] = self
 
     def __getattr__(self, name):
         return getattr(self.client, name)
+
+
+class DisposableEmail(object):
+    """
+    Validates an email address.
+
+    :param message:
+        Error message to raise in case of a validation error.
+    """
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        domain = field.data.rsplit('@', 1)[-1]
+        is_disposable = current_app.extensions['bdea'].get_domain_status(domain).is_disposable()
+        if is_disposable:
+            message = self.message
+            if message is None:
+                message = field.gettext('Unacceptable email address.')
+            raise ValidationError(message)
 
 
 # EOF
